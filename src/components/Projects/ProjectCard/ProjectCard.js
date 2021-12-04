8b896d6c65898ed3341c7e31a202b5669ae4377a
optimm <ayushsaxena823@gmail.com>
2021-12-05 00:59:18 +0530

    project added

--- src/components/Projects/ProjectCard/ProjectCard.js
+++ src/components/Projects/ProjectCard/ProjectCard.js
@@ -11,7 +11,7 @@ function ProjectCard() {
   return (
     <>
       {ProjectList.map((list, index) => (
-        <Card key={index}>
+        <Card key={index} className="wow bounceInUp">
           <CardLeft>
             <img src={list.img} alt={list.name} />
           </CardLeft>
