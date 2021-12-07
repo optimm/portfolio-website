36c2f9c4e1a6bb96b9fb66a22a31010b40262b04
optimm <ayushsaxena823@gmail.com>
2021-12-08 02:38:03 +0530

    bottom social icons

--- src/components/SocialIcon/SocialIcon.js
+++ src/components/SocialIcon/SocialIcon.js
@@ -37,7 +37,7 @@ function SocialIcon() {
       <ul>
         <li className="item">
           <a
-            href="https://www.linkedin.com/in/your_username/"
+            href="https://www.linkedin.com/in/ayush-saxena-b5b099203/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -46,7 +46,7 @@ function SocialIcon() {
         </li>
         <li className="item">
           <a
-            href="https://github.com/your_username/"
+            href="https://github.com/optimm"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -55,7 +55,7 @@ function SocialIcon() {
         </li>
         <li className="item">
           <a
-            href="https://github.com/your_username/"
+            href="https://www.instagram.com/ayushsaxenaa__/"
             target="_blank"
             rel="noopener noreferrer"
           >
